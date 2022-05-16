9100f2294e143c7ebf3ad6a332cd2bc48069e133
Mayank <star@gmail.com>
2022-05-16 15:41:46 +0530

    first azure

---
+++ src/test/java/org/facebook/loginpage/VerificationAzure.java
@@ -0,0 +1,34 @@
+package org.facebook.loginpage;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Facebook.Testing.LaunchBrowser;
+
+public class VerificationAzure extends LaunchBrowser {
+
+	public WebDriver driver;
+	@BeforeTest
+	public void initialFlow() {
+	driver = Browserselection();
+	}
+	@Test
+	public void testAzure() {
+		driver.get("https://mayankdemo.azurewebsites.net/webapp/");
+		String urldata = driver.getCurrentUrl();
+		Assert.assertEquals(urldata, "https://mayankdemo.azurewebsites.net/webapp/");
+		
+		
+	}
+	@AfterTest
+	public void closeBrowser() {
+		driver.quit();
+	}
+	
+}
+	
+		
+	
